3b082a0a228731791ae9e0ef16dc6c74b54d4fc1
Ivan Lisin <77109733+ivanlisin@users.noreply.github.com>
2020-11-30 14:36:13 +0700

    fix lint err & update test

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,14 +1,8 @@
-import { test, expect } from '@jest/globals';
 import path from 'path';
 import fs from 'fs';
-import { fileURLToPath } from 'url';
 import message from '..';
 
-/* eslint no-underscore-dangle: [2, { "allow": ["__filename", "__dirname"] }] */
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const getFixturePath = (filename) => path.join('__fixtures__', filename);
 const readFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
 
 test('', () => {
